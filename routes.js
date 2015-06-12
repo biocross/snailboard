794e63a71a8a8145b915b104419675dd3dc5f2f4
Siddharth Gupta <sids.1992@gmail.com>
2015-06-12 14:46:34 +0530

    Init Code
    - Basic Navigation + Feed Navigation
    - Just plain UI

---
+++ routes.js
@@ -0,0 +1,17 @@
+Router.configure({
+  // we use the  appBody template to define the layout for the entire app
+  // the appNotFound template is used for unknown routes and missing lists
+  notFoundTemplate: 'NotFound',
+
+  // show the appLoading template whilst the subscriptions below load their data
+  loadingTemplate: 'loading',
+
+  // wait on the following subscriptions before rendering the page to ensure
+  // the data it's expecting is present
+});
+
+
+Router.route('/', function(){
+  //GAnalytics.pageview();
+  this.render('Home');
+});
